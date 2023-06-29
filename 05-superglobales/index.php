70a4917c07bf2ba5b09f759d3d7b2d78c9a127d9
VULQUIN Jérôme <gizmals@gmail.com>
2023-06-29 09:36:22 +0200

    AJout cours SuperGlobales GET & POST

--- 05-superglobales/index.php
+++ 05-superglobales/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Superglobales</title>
 </head>
 
@@ -18,7 +19,7 @@
     $name = $_GET['name'] ?? null; // Si $_GET['name'] n'existe pas alors $name sera null
     $name = $_GET['name'] ?? 'John'; // Si $_GET['name'] n'existe pas alors $name sera John
     $age = $_GET['age'] ?? null;
-    $age = (int) $age;
+    $age = (int) $age; // ici on CAST le string $age en integer (transforme)
     var_dump($age);
     ?>
 
@@ -31,18 +32,42 @@
     <?php if ($age) { ?>
         <p>Tu as <?= $age; ?> ans.</p>
     <?php } ?>
-
+    
+    <h2>Formulaire en GET</h2>
     <form action="" method="GET">
-        <input type="text" name="name" value="<?= $name ?>" id="">
-        <select name="age">
-            <?php for ($i = 18; $i <= 120; $i++) { ?>
-                <option value="<?= $i ?>" <?= $i == $age ? 'selected' : '' ?>>
-                    <?= $i . " ans" ?>
-                </option>
-            <?php } ?>
-        </select>
+        <div class="section">
+            <label for="NOM">Nom</label>
+            <input type="text" name="name" id="NOM" value="<?= $name ?>" id="">
+        </div>
+
+        <div class="section">
+            <label for="AGE"></label>
+            <select name="age" id="AGE">
+                <?php for ($i = 18; $i <= 120; $i++) { ?>
+                    <option value="<?= $i ?>" <?= $i == $age ? 'selected' : '' ?>>
+                        <?= $i . " ans" ?>
+                    </option>
+                <?php } ?>
+            </select>
+        </div>
         <button>Envoyer</button>
     </form>
+
+    <h2>Formulaire en POST</h2>
+    <form action="" method="POST">
+        <div class="section">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button>Envoyer</button>
+    </form>
+    <?php
+    var_dump($_POST);
+    $password = $_POST['password'] ?? null;
+    if ($password) {
+        echo "<p>Votre mot de passe: $password</p>";
+    }
+    ?>
 </body>
 
 </html>
